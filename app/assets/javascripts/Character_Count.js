4f35881d2f2c324197b23e693fa650e46262c056
Harry Chapman <HyarcherHyarcher@gmail.com>
2016-03-23 10:05:44 +0000

    some changes

--- app/assets/javascripts/Character_Count.js
+++ app/assets/javascripts/Character_Count.js
@@ -6,8 +6,41 @@ function updateCountdown() {
 function moneyforletters() {
     var typed = 0.05* jQuery('.message').val().length
     cashconvert = typed.toFixed(2);
-    $('#blog_fee').text(cashconvert);
+    jQuery('#blog_fee').val(cashconvert);
 }
+counter = function() {
+    var value = $('#text').val();
+
+    if (value.length == 0) {
+        $('#wordCount').html(0);
+        $('#totalChars').html(0);
+        $('#charCount').html(0);
+        $('#charCountNoSpace').html(0);
+        $('#maxCharCount').html(140);
+        return;
+    }
+
+    var regex = /\s+/gi;
+    var wordCount = value.trim().replace(regex, ' ').split(' ').length;
+    var totalChars = value.length;
+    var charCount = value.trim().length;
+    var charCountNoSpace = value.replace(regex, '').length;
+
+    $('#wordCount').html(wordCount);
+    $('#totalChars').html(totalChars);
+    $('#charCount').html(charCount);
+    $('#charCountNoSpace').html(charCountNoSpace);
+};
+
+$(document).ready(function() {
+    $('#count').click(counter);
+    $('#text').change(counter);
+    $('#text').keydown(counter);
+    $('#text').keypress(counter);
+    $('#text').keyup(counter);
+    $('#text').blur(counter);
+    $('#text').focus(counter);
+});
 
 jQuery(document).ready(function($) {
     updateCountdown();
